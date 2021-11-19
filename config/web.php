49f2f7643cfc6182121322c59698d2a95f69bf2f
poi <rbarguellesjr@sec.gov.ph>
2021-11-19 09:40:30 +0800

    update

--- config/web.php
+++ config/web.php
@@ -80,6 +80,9 @@ $config = [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'OPRkSxrK4jC5YcyGeF4tiZ5cyEtpo3ei',
+            'parsers' => [
+                'application/json' => 'yii\web\JsonParser',
+            ]
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
