fa1d962df49e003dcc0b25f762dcf90905bc0fbb
poi <rbarguellesjr@sec.gov.ph>
2021-11-29 09:41:08 +0800

    update

--- controllers/ApiController.php
+++ controllers/ApiController.php
@@ -196,7 +196,7 @@ class ApiController extends ActiveController
                         $model = \app\models\TblNegativeList::findOne(['fld_sec_reg_no_fk'=>$data['sec_reg_no'],'fld_status_code_fk'=>134,'fld_cleared'=>0]);
                         if($model == null)
                         {
-                            $data['remarks'] = 'For Post Audit';
+                            $data['remarks'] = 'Subject to Post-Audit';
                             $this->addInfraction($data);
                         }
                     }
